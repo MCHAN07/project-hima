de0aa48ce67071e541dc0163d5b1341468aca15d
MCHAN7 <mchandra7733@gmail.com>
2025-02-21 10:48:08 +0700

    new

---
+++ src/page/index.jsx
@@ -0,0 +1,317 @@
+import React from "react";
+import "./index.css";
+
+export default function Main() {
+  return (
+    <div className="main-container">
+      <div className="rectangle">
+        <div className="flex-row-cd">
+          <span className="tentang-kami">Tentang Kami</span>
+          <span className="kontak">Kontak</span>
+          <div className="regroup">
+            <span className="home">Home</span>
+            <span className="sejarah">Sejarah</span>
+          </div>
+        </div>
+        <div className="flex-row-fe">
+          <span className="himpunan-mahasiswa-bisnis-digital">
+            Himpunan Mahasiswa <br />
+            Bisnis Digital
+          </span>
+          <span className="universitas-pgri-semarang">
+            Universitas PGRI Semarang
+          </span>
+          <div className="logo-baru-bisdig-vb" />
+          <div className="logo-upgris" />
+          <div className="empty" />
+          <div className="rectangle-1">
+            <span className="visi-misi">Visi & Misi</span>
+          </div>
+          <div className="rectangle-2">
+            <span className="tentang-kami-3">Tentang Kami</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex-row-df">
+        <div className="rectangle-4" />
+        <span className="himpunan-mahasiswa-bisnis-digital-5">
+          Himpunan Mahasiswa <br />
+          Bisnis Digital
+        </span>
+        <div className="img" />
+        <div className="img-6" />
+        <div className="rectangle-7">
+          <span className="empty-8"> 14</span>
+          <span className="program-kerja">Program Kerja</span>
+        </div>
+        <div className="rectangle-9" />
+        <div className="rectangle-a" />
+        <div className="rectangle-b">
+          <span className="empty-c">17</span>
+          <span className="agenda">Agenda</span>
+        </div>
+        <div className="rectangle-d">
+          <span className="empty-e">21</span>
+          <span className="anggota">Anggota</span>
+          <div className="image">
+            <div className="img-f" />
+            <div className="img-10" />
+            <div className="img-11" />
+          </div>
+        </div>
+        <div className="rectangle-12">
+          <span className="david-risyad-raffi">David Risyad Raffi</span>
+          <span className="wakil-ketua-umum">Wakil Ketua Umum</span>
+        </div>
+        <span className="himpunan-mahasiswa-bisnis-digital-13">
+          Himpunan Mahasiswa Bisnis Digital adalah organisasi yang mewadahi
+          mahasiswa dari program studi Bisnis Digital. Memiliki tujuan untuk
+          mengembangkan potensi anggotanya melalui kegiatan akademik, sosial,
+          dan kemasyarakat serta menjadi wadah bagi apresiasi kepentingan
+          mahasiswa dilingkungan kampus
+        </span>
+        <div className="rectangle-14">
+          <span className="gatut-zakka-habibi-w">Gatut Zakka Habibi W</span>
+          <span className="ketua-umum">Ketua Umum</span>
+        </div>
+        <div className="rectangle-15">
+          <span className="read-more">Read More</span>
+        </div>
+      </div>
+      <span className="struktur-organisasi">
+        Struktur Organisasi
+        <br />
+        Himpunan Mahasiswa Bisnis Digital
+      </span>
+      <div className="rectangle-16">
+        <span className="ketua-umum-17">Ketua Umum</span>
+      </div>
+      <div className="gambar-whatsapp">
+        <div className="rectangle-18">
+          <span className="go-digital-beyond">
+            Go Digital Beyond To Succses
+          </span>
+        </div>
+      </div>
+      <div className="flex-row">
+        <span className="kegiatan-himpunan-mahasiswa">
+          Kegiatan Himpunan Mahasiswa Bisnis Digital
+        </span>
+        <span className="himpunan-mahasiswa-bisnis-digital-19">
+          Himpunan Mahasiswa Bisnis Digital telah melakukan berbagai kegiatan
+          yang bisa anda lihat di bawah ini
+        </span>
+        <div className="rectangle-1a">
+          <span className="view-all">View All </span>
+        </div>
+      </div>
+      <div className="flex-row-c">
+        <div className="rectangle-1b">
+          <span className="maret">03 Maret 2024</span>
+          <div className="rectangle-1c">
+            <span className="read-more-1d">Read More</span>
+          </div>
+          <div className="gambar-whatsapp-1e" />
+          <div className="rectangle-1f">
+            <span className="raker-upgrading">Raker & Upgrading</span>
+          </div>
+        </div>
+        <div className="rectangle-20">
+          <span className="mei">10 Mei 2024</span>
+          <div className="rectangle-21">
+            <span className="read-more-22">Read More</span>
+          </div>
+          <div className="gambar-whatsapp-23" />
+          <div className="rectangle-24">
+            <span className="gathering">Gathering</span>
+          </div>
+        </div>
+        <div className="rectangle-25">
+          <span className="april">16 April 2024</span>
+          <div className="rectangle-26">
+            <span className="read-more-27">Read More</span>
+          </div>
+          <div className="gambar-whatsapp-28" />
+          <div className="rectangle-29">
+            <span className="diprodi">Diprodi</span>
+          </div>
+        </div>
+        <div className="vector" />
+        <div className="vector-2a" />
+        <span className="rapat-kerja">
+          Rapat Kerja (Raker) dan Upgrading Himpunan Mahasiswa bertujuan untuk
+          menyusun dan merencanakan program kerja, mengevaluasi kinerja,
+          memastikan koordinasi antar departemen, menyusun anggaran, dan
+          menetapkan kebijakan internal.{" "}
+        </span>
+        <span className="gathering-hima-bisdig">
+          Gathering Hima Bisdig bertujuan untuk menjaga silahturahmi kepada
+          semua anggota dan menambah keakraban anggota
+        </span>
+        <span className="diprodi-dialog">
+          Diprodi adalah dialog program studi yang bertujuan sebagaui wadah
+          mahasiswa menyampaikan aspirasinya kepada para dosennya langsung
+        </span>
+      </div>
+      <div className="rectangle-2b">
+        <div className="flex-row-df-2c">
+          <span className="bidang-minat-bakat">Bidang Minat Bakat</span>
+          <div className="rectangle-2d">
+            <span className="view-all-2e">View All </span>
+          </div>
+          <div className="img-2f" />
+          <span className="ketua-bidang">Ketua Bidang</span>
+          <div className="img-30">
+            <span className="sekretaris-bidang">Sekretaris Bidang</span>
+          </div>
+          <div className="img-31" />
+          <span className="staff-bidang">Staff Bidang</span>
+          <div className="rectangle-32">
+            <span className="lorem-ipsum">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at
+              massa sit amet nisi blandit vehicula adipiscing elit.{" "}
+            </span>
+          </div>
+          <div className="vector-33" />
+          <div className="vector-34" />
+        </div>
+        <span className="putri-kusumawati">
+          Putri Kusumawati - Hidayatul Fajar - Ristina Ayu
+        </span>
+      </div>
+      <div className="flex-row-c-35">
+        <span className="informasi-lainnya-bisnis-digital">
+          Informasi Lainnya
+          <br />
+          Bisnis Digital
+        </span>
+        <span className="informasi-lainnya-seputar-program-studi">
+          Informasi lainnya seputar tentang prohgram studi bisnis digital bisa
+          kalian lihat di bawah ini
+        </span>
+        <div className="rectangle-36">
+          <span className="view-all-37">View All </span>
+        </div>
+      </div>
+      <div className="flex-row-d">
+        <div className="rectangle-38">
+          <div className="gambar-whatsapp-39" />
+          <div className="rectangle-3a">
+            <span className="pmb-bisnis-digital">PMB Bisnis Digital</span>
+          </div>
+          <span className="melayani-pendaftaran">
+            Melayani pendaftaran bagi mahasiswa yang ingin mendaftar di prodi
+            bisnis digital
+          </span>
+          <div className="rectangle-3b">
+            <span className="read-more-3c">Read More</span>
+          </div>
+        </div>
+        <div className="gambar-whatsapp-3d" />
+        <div className="rectangle-3e">
+          <div className="rectangle-3f">
+            <span className="juara-taekwondo">Juara 2 Taekwondo</span>
+          </div>
+          <span className="tabah-habib-soeharto">
+            Tabah Habib Soeharto pemenang juara ke 2 lomba taekwondo dalam ajang
+            porsema{" "}
+          </span>
+          <div className="rectangle-40">
+            <span className="read-more-41">Read More</span>
+          </div>
+        </div>
+        <div className="rectangle-42">
+          <div className="rectangle-43">
+            <span className="uang-tahun-prodi">Uang Tahun Prodi</span>
+          </div>
+          <div className="rectangle-44">
+            <span className="read-more-45">Read More</span>
+          </div>
+        </div>
+        <div className="gambar-whatsapp-46" />
+        <div className="vector-47" />
+        <div className="vector-48" />
+        <span className="tanggal-juli">
+          Tanggal 13 juli adalah tanggal bersejarah sekaligus hari yang istimewa
+          untuk prodi bisnis digital upgris
+        </span>
+      </div>
+      <span className="ayo-gabung">
+        Ayo gabung bersama kami
+        <br />
+        di Prodi Bisnis Digital
+      </span>
+      <span className="program-studi-bisnis-digital">
+        Program Studi Bisnis Digital mempersiapkan mahasiswa menghadapi era
+        digital dengan menggabungkan ilmu bisnis dan teknologi.
+      </span>
+      <div className="rectangle-49">
+        <span className="join-now">Join Now</span>
+      </div>
+      <div className="rectangle-4a">
+        <span className="copyright-himabisdigupgris">
+          Copyright © 2024 Himabisdigupgris
+        </span>
+        <div className="flex-row-b">
+          <span className="location">Location</span>
+          <span className="contact-us">Contact Us</span>
+          <div className="gambar-whatsapp-4b" />
+          <span className="instagram-himabisdigupgris">
+            Instagram : himabisdigupgris
+          </span>
+          <span className="address">
+            Jl. Sidodadi Timur No. 24,
+            <br />
+            Karangtempel, Kec. Semarang Timur, <br />
+            Kota Semarang, Jawa Tengah <br />
+            50232
+          </span>
+          <span className="youtube-himabisdigupgris">
+            Youtube : himabisdigupgris
+          </span>
+          <span className="email-himabisdigupgris">
+            Email :himabisdigupgris@gmail.com
+          </span>
+          <span className="tiktok-himabisdigupgris">
+            Tiktok :himabisdigupgris
+          </span>
+        </div>
+        <span className="saran-masukkan">Saran & Masukkan</span>
+      </div>
+      <div className="gambar-whatsapp-4c" />
+      <div className="flex-row-e">
+        <div className="line" />
+        <div className="rectangle-4d">
+          <span className="ketua-umum-4e">Wakil Ketua Umum</span>
+        </div>
+        <div className="line-4f" />
+      </div>
+      <div className="flex-row-baaa">
+        <div className="rectangle-50">
+          <span className="sekertaris-umum">Sekertaris Umum</span>
+        </div>
+        <div className="bendahara-umum">
+          <span className="ketua-umum-51">Bendahara Umum</span>
+        </div>
+      </div>
+      <div className="flex-row-f">
+        <div className="rectangle-52">
+          <span className="bidang-shosum">Bidang Shosum</span>
+        </div>
+        <div className="bidang-kominfo">
+          <span className="bidang-sdm">Bidang Kominfo</span>
+        </div>
+        <div className="bidang-minat-bakat-53">
+          <span className="bidang-keilmuan">Bidang SDM</span>
+        </div>
+        <div className="rectangle-54">
+          <span className="ketua-umum-55">Bidang Minat Bakat</span>
+        </div>
+        <div className="ketua-umum-56">
+          <span className="ketua-umum-57">Bidang Keilmuan</span>
+        </div>
+        <div className="line-58" />
+      </div>
+    </div>
+  );
+}
